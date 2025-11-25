5d0b0ef38f7b82f034b4d70b126e9ae0f7a1e43e
clarliteratuscoc-cyber <clar.literatus.coc@phinmaed.com>
2025-11-25 19:51:43 +0800

    GithubLiteratus

---
+++ GithubLiteratus.py
@@ -0,0 +1,9 @@
+PS C:\githubexample> git init
+Initialized empty Git repository in C:/githubexample/.git/
+PS C:\githubexample> git add .
+PS C:\githubexample> git commit -m "example"
+[main (root-commit) 21df7a0] example
+ 1 file changed, 11 insertions(+)
+ create mode 100644 example.py
+PS C:\githubexample> git remote add origin https://github.com/missarchi/examplegithub
+PS C:\githubexample> git branch -M main
